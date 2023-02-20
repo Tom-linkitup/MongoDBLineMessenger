19af880e74bd385530cd789ece2104bee2116e26
tomgu <atom8708@gmail.com>
2023-02-20 23:18:01 +0800

    setup a Sprint Boot project

---
+++ src/main/java/com/example/mongodblinemessenger/MongoDbLineMessengerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongodblinemessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbLineMessengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoDbLineMessengerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mongodblinemessenger/MongoDbLineMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mongodblinemessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbLineMessengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
